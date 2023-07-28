d50d827f765221a0772f4e416aa7a3b77b05e3fc
NouranHussein <nouranhussein1999@gmail.com>
2023-07-28 16:44:50 +0300

    Adding Introduction files

---
+++ introduction.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"introduction.h"
+
+ int main()
+{
+    std::string input;
+    std::cout<<"Enter Your Name: \n\n";
+    std::cin>>input;
+    std::cout<<"your name: "<<input;
+
+    return 0;
+}
